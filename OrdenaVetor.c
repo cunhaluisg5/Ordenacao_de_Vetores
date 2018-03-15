244b1d99946ae427cc618ff907fceb16c4b3e037
Luis <cunhaluisg5@gmail.com>
2018-03-15 11:36:11 -0300

    Adicionada função Alocar

--- OrdenaVetor.c
+++ OrdenaVetor.c
@@ -2,7 +2,24 @@
 #include<stdlib.h>
 #include<time.h>
 
+int *alocar(int n);
+
 int main()
 {
 	
+}
+
+int *alocar(int n)
+{
+	int *v;
+	if(n < 1){
+    	printf("** Erro: Parametro invalido **\n");
+    	return(NULL);
+ 	}
+  	v = (int *) malloc (n * sizeof(int));
+	if(v == NULL){
+    	printf("** Erro: Memoria Insuficiente **");
+    	return(NULL);
+	}     
+	return(v);
 }
