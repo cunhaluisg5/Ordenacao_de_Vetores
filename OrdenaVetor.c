7c2e5b7630a8d600cc0c15b96d9ddf362c60ac76
cunhaluisg5 <cunhaluisg5@gmail.com>
2018-03-15 12:10:31 -0300

    Adicionada função MergeSort

--- OrdenaVetor.c
+++ OrdenaVetor.c
@@ -12,6 +12,8 @@ void selecao(int *v, int n);
 void insercao(int *v, int n);
 void shellsort(int *v, int n);
 void quicksort(int *v, int n);
+void merge(int *v, int inicio, int meio, int fim);
+void mergeSort(int *v, int inicio, int fim);
 
 int main()
 {
@@ -180,4 +182,60 @@ void quicksort(int *v, int n){
 	v[b] = x;
 	quicksort(v, b);
 	quicksort(&v[a], n - a);
+}
+
+void merge(int *v, int inicio, int meio, int fim)
+{
+    int i, j, k;
+    int n1 = meio - inicio + 1;
+    int n2 =  fim - meio;
+    int L[n1], R[n2];    
+    for(i = 0; i < n1; i++)
+        L[i] = v[inicio + i];
+    for(j = 0; j < n2; j++)
+        R[j] = v[meio + 1 + j]; 
+    i = 0; 
+    j = 0;
+    k = inicio; 
+    while(i < n1 && j < n2)
+    {
+    	comparacoes++;
+        if(L[i] <= R[j])
+        {
+        	movimentacoes++;
+            v[k] = L[i];
+            i++;
+        }
+        else
+        {
+        	movimentacoes++;
+            v[k] = R[j];
+            j++;
+        }
+        k++;
+    } 
+    while(i < n1)
+    {
+    	movimentacoes++;
+        v[k] = L[i];
+        i++;
+        k++;
+    } 
+    while(j < n2)
+    {
+    	movimentacoes++;
+        v[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int *v, int inicio, int fim) {
+	comparacoes++;
+	if(inicio < fim) {
+		int meio = (inicio + fim) / 2;
+		mergeSort(v, inicio, meio);
+		mergeSort(v, meio + 1, fim);
+		merge(v, inicio, meio, fim);
+	}
 }
